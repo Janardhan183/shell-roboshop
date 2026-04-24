a3d0fda0717ccd5c3acb15b4b08515cdcfa1eb9a
Jana Vankam <janamec315@gmail.com>
2026-04-24 11:06:32 +0530

    loop messages

---
+++ roboshop.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+AMI_ID=""
+SG_ID=""
+
+for instance in $@
+do
+    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-0220d79f3f480ecf5 --instance-type t3.micro --security-group-ids sg-0230fed75339e6e0f --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]"` --query 'Instances[0].InstanceId' --output text)
+    
+    #Get Private IP
+    if [ $instance != "frontend" ]; then
+       IP=$(aws ec2 describe-instances --instance-ids i-08624cfb8c93215dd --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
+    else 
+       IP=$(aws ec2 describe-instances --instance-ids i-08624cfb8c93215dd --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
+    fi    
+      echo "$instance: $IP"
+done
